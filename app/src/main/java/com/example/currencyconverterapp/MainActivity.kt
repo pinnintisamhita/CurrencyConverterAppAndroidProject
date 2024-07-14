b573e794546befb27daeab15ba40b997aa24210a
samhita <pinnintisamhitha72@gmail.com>
2024-07-14 12:01:25 +0530

    added a condition

--- app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverterapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.currencyconverterapp
 
+import android.health.connect.datatypes.units.Length
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,18 +19,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-          editText=findViewById(R.id.editText)
-          button=findViewById(R.id.btn)
-          textView=findViewById(R.id.ResultText)
-          button.setOnClickListener {
-              var enteredtext:String=editText.text.toString()
-              var indianRupees:Double=enteredtext.toDouble()
-              var usd=currencyConverter(indianRupees)
-              textView.text= "$usd Dollars"
-          }
+        editText = findViewById(R.id.editText)
+        button = findViewById(R.id.btn)
+        textView = findViewById(R.id.ResultText)
+        button.setOnClickListener {
+            var enteredtext: String = editText.text.toString()
+            if (enteredtext == "") {
+                Toast.makeText(this, "please enter the Indian currency  value", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                var indianRupees: Double = enteredtext.toDouble()
+                var usd = currencyConverter(indianRupees)
+                textView.text = "$usd Dollars"
+
+            }
+            editText.text.clear()
         }
-       fun currencyConverter(p:Double):Double
-       {
-           return p*0.012
-       }
+
     }
+    fun currencyConverter(p: Double): Double {
+        return p * 0.012
+    }
+}
